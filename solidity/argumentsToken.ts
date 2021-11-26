13a9182a9aa3642a581d4c5c8124ecc702999506
IntegralTeam <repo@sfxdx.ru>
2021-11-26 15:13:55 +0700

    add verify args for token

---
+++ solidity/argumentsToken.ts
@@ -0,0 +1,10 @@
+const args: Array<any> = [
+    "0xD0e8334B3539d0Fc7098d8facB66b55247c1472c",
+    "test",
+    "wfund",
+    "9",
+    "120000000000000000",
+    "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"
+];
+
+export default args
